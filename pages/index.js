a3462bbcf32d6e782c3865d8833f7e50f8ae6f11
github_username <princekumarsharma365@gmail.com>
2023-07-17 23:43:57 +0530

    product page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 
 import HeadMeta from '@components/headMeta/HeadMeta'; 
-import Header from '@components/header/Header'
+import Header from '@components/header/Header';
+import Product from '@components/product/Product';
 
 export default function Home() {
   return (
     <div className="container">
       <HeadMeta pageTitle="home page"/>
       <Header />
+      <Product/>
     </div>
   )
 }
